2054b2f1efdb93e8bc3f5dc7402eb10fe11aad76
Rabindra Basnet <rabindrabasnet82@gmail.com>
2023-05-09 23:53:01 +0545

    name.py and school.txt was modified

--- name.py
+++ name.py
@@ -1,4 +1,4 @@
 print("hello")
 
-name = f"{this provides the great opportunity for those individual while learning}"
+name = f'this provides the great opportunity for those individual while learning'
 print(name)
